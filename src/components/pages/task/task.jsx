9ec1d4300969849a3e09cf25eaa310c54bda2204
tnsska2 <vnemftnsska2@naver.com>
2022-01-05 23:58:01 +0900

    grid size

--- src/components/pages/task/task.jsx
+++ src/components/pages/task/task.jsx
@@ -21,7 +21,7 @@ const Task = ({}) => {
       <Grid container spacing={2}>
         {[...Array(10)].map(v => {
           return (
-            <Grid item xs={12} md={3}>
+            <Grid item xs={12} md={6} lg={3}>
               <TaskMemo></TaskMemo>
             </Grid>
           )
