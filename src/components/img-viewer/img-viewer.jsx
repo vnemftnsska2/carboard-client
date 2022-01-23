28bd63467da9baad8840a92243b8b15415636915
tnsska2 <vnemftnsska2@naver.com>
2022-01-23 14:45:28 +0900

    Mod: Img Viewer Size

--- src/components/img-viewer/img-viewer.jsx
+++ src/components/img-viewer/img-viewer.jsx
@@ -7,6 +7,7 @@ const ImgViewer = ({open, fileName, handleClose}) => {
       <DialogContent>
         <img
           src={`${process.env.REACT_APP_API_SERVER || 'http://localhost:3030'}/image/${fileName}`}
+          width={1000}
           alt={fileName}
         />
       </DialogContent>
