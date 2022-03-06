21f248c018dbb647926a223272d149b0c1826b2e
tnsska2 <vnemftnsska2@naver.com>
2022-03-06 16:21:03 +0900

    Feat: 추가. 상품등록 및 가격 정보 변경 페이지 UI구성 및 DB
    - pricing.jsx: 페이지 기획 및 레이아웃 구성
    - DB: 추가. procuct_brand 테이블 생성 및 스키마 구성

--- src/pages/pricing/pricing.jsx
+++ src/pages/pricing/pricing.jsx
@@ -1,38 +1,131 @@
-import { Box, Paper, Grid, Stack, TextField, Button } from "@mui/material";
+import { Box, Paper, Grid, Button, Stack } from "@mui/material";
 import React from "react";
 
+//DataGrid
+import { DataGrid } from "@mui/x-data-grid";
+
+const productRows = [
+  {
+    id: 0,
+    brand: "레이노",
+    description: "필름 취급",
+  },
+  {
+    id: 1,
+    brand: "블랙박스브랜드",
+    description: "블랙박스만 취급",
+  },
+  {
+    id: 2,
+    brand: "브이클",
+    description: "설명",
+  },
+  {
+    id: 3,
+    brand: "버텍스",
+    description: "설명",
+  },
+  {
+    id: 4,
+    brand: "파인뷰",
+    description: "설명",
+  },
+];
+
+const unitRows = [
+  {
+    id: 0,
+    product: "S5-5%",
+    price: "100000",
+    description: "설명",
+  },
+  {
+    id: 1,
+    product: "S5-15%",
+    price: "100000",
+    description: "설명",
+  },
+  {
+    id: 2,
+    product: "S7-35%",
+    price: "100000",
+    description: "설명",
+  },
+];
+
 const Pricing = (props) => {
+  //Table
+  const productColumns = [
+    { field: "brand", headerName: "브랜드", width: "20%" },
+    { field: "description", headerName: "설명", width: "65%" },
+    { field: "", headerName: "", width: "15%" },
+  ];
+
+  const unitColumns = [
+    { field: "product", headerName: "상품", width: 150 },
+    { field: "price", headerName: "가격", width: 200 },
+    { field: "description", headerName: "설명", width: 400 },
+    { field: "", headerName: "", width: 120 },
+  ];
+
   return (
     <Box>
-      <Paper sx={{ marginTop: "10px", padding: "1em 3em 1em 1.5em" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={6} lg={4}>
-            <Stack direction="row" alignItems="flex-end">
-              <TextField
-                label="검색"
-                type="search"
-                variant="standard"
-                sx={{ minWidth: 150 }}
-                inputRef={null}
-                onKeyDown={() => {}}
-              />
-              <Button size="small" variant="outlined" onClick={() => {}}>
-                검색
+      <Grid container spacing={2}>
+        <Grid item xs={4} md={4} lg={4}>
+          <Paper sx={{ marginTop: "1em", padding: "1em" }}>
+            <Stack
+              mb={2}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{ padding: "0 0.3em 0 0.3em" }}
+            >
+              <font size="5">
+                <strong>브랜드</strong>
+              </font>
+              <Button
+                variant="contained"
+                sx={{ width: "5em", textAlign: "right" }}
+                onClick={() => {}}
+              >
+                등록
               </Button>
             </Stack>
-          </Grid>
-          <Grid item xs={6} md={6} lg={8} sx={{ textAlign: "right" }}>
-            <Button
-              ref={null}
-              variant="contained"
-              sx={{ minWidth: "7em", marginTop: "12px" }}
-              onClick={() => {}}
-            >
-              업체 등록
-            </Button>
-          </Grid>
+            <DataGrid
+              columns={productColumns}
+              rows={productRows}
+              sx={{ height: "80vh" }}
+            />
+          </Paper>
         </Grid>
-      </Paper>
+        <Grid item xs={8} md={8} lg={8}>
+          <Paper sx={{ marginTop: "1em", padding: "1em" }}>
+            <Stack
+              mb={2}
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{ padding: "0 0.3em 0 0.3em" }}
+            >
+              <font size="5">
+                <strong>레이노</strong>
+              </font>
+              <Button
+                variant="contained"
+                sx={{ width: "5em", textAlign: "right" }}
+                onClick={() => {}}
+              >
+                추가
+              </Button>
+            </Stack>
+            <DataGrid
+              columns={unitColumns}
+              rows={unitRows}
+              sx={{ height: "80vh" }}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
